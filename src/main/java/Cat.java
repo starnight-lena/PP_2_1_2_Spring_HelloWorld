b07f6556f62f77b6464fbd889b71e30b1f8b4c69
Elena Kiseleva <elena.kiseleva91@yandex.ru>
2022-12-14 12:01:55 +0500

    #Commit 1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name) {
+        this.name = name;
+    }
+}
